112886be6ac16add517f1da01c021f8b6da8fea1
zef-nomer <80211406+zef-nomer@users.noreply.github.com>
2021-12-30 14:45:49 +0300

    Added User-Agent supporting.
    Yes <3

--- functions.py
+++ functions.py
@@ -1,20 +1,46 @@
-import wget
+import requests
+import webbrowser
 import os
 import time
 from PIL import Image
 
 
 def head():
+
     nick = input("Nickname: ")
+
+    if os.path.exists("agent.txt"):
+        pass
+    else:
+        webbrowser.open('https://www.whatsmyua.info/', new=2)
+        agent = input(
+            "Your User-Agent(Your default browser will open now, you should copy your User-Agent. After, insert it here. We don't steal your data!)")
+
+        file = open("agent.txt", "wb")
+        file.write(agent)
+        file.close()
+
+    with open("agent.txt", "r") as txt:
+        headers = {
+            'User-Agent': f'{txt}'
+        }
+        print(headers)
+
     url = f"https://tlauncher.org/upload/all/nickname/{nick}.png"
+
     name = url.split("/")
     name = name[len(name)-1]
-    #print(f"Skin will be saved as {name} in {os.getcwd()}")
+
+    print(f"Skin will be saved as {name} in {os.getcwd()}")
+
     if os.path.exists(name):
         os.remove(name)
-    try:
-        wget.download(url, name)
 
+    try:
+        response = requests.get(url, headers=headers)
+        file = open(f"{nick}.png", "wb")
+        file.write(response.content)
+        file.close()
     except:
         print(f"Nickname {nick} not found")
         print("")
@@ -29,23 +55,45 @@ def head():
     mask_im = Image.open('crop2.png').resize(im_crop1.size).convert('RGBA')
     im_crop1.paste(im_crop2, mask_im)
     im_crop1.save(f'head-{name}')
+
     os.remove("crop2.png")
     os.remove(name)
 
 
 def skin():
     nick = input("Nickname: ")
-    url = f"https://tlauncher.org/upload/all/nickname/{nick}.png"
+
+    if os.path.exists("agent.txt"):
+        pass
+    else:
+        webbrowser.open('https://www.whatsmyua.info/', new=2)
+        agent = input(
+            "Your User-Agent (Your default browser will open now, you should copy your User-Agent. After, insert it here. We don't steal your data!): ")
+
+        file = open("agent.txt", "wb")
+        file.write(agent.encode("utf-8"))
+        file.close()
+
+    with open("agent.txt", "r") as txt:
+        headers = {
+            'User-Agent': f'{txt}'
+        }
+        print(headers)
+
+    url = f"https://tlauncher.org/upload/all/nickname/tlauncher_{nick}.png"
     name = url.split("/")
     name = name[len(name)-1]
+
     print(f"Skin will be saved as {name}")
 
     if os.path.exists(name):
         os.remove(name)
-    try:
-        wget.download(url, name)
-        print("\n")
 
+    try:
+        response = requests.get(url, headers=headers)
+        file = open(f"{nick}.png", "wb")
+        file.write(response.content)
+        file.close()
     except:
         print(f"Nickname {nick} not found")
         print("")
